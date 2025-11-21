a56616362a69a67608dc5cab1da2444c31e5474b
chaymae-bayousfi <chaymae.bayousfi@gmail.com>
2025-11-21 12:05:44 +0100

    cub3d

---
+++ parsing/load_tex.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   load_tex.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbayousf <cbayousf@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/18 17:32:17 by haqajjef          #+#    #+#             */
+/*   Updated: 2025/10/30 10:42:53 by cbayousf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub.h"
+
+void	destroy_textures(t_parse *parse)
+{
+	if (parse->textures[NO].tex)
+	{
+		mlx_delete_texture(parse->textures[NO].tex);
+		parse->textures[NO].tex = NULL;
+	}
+	if (parse->textures[SO].tex)
+	{
+		mlx_delete_texture(parse->textures[SO].tex);
+		parse->textures[SO].tex = NULL;
+	}
+	if (parse->textures[WE].tex)
+	{
+		mlx_delete_texture(parse->textures[WE].tex);
+		parse->textures[WE].tex = NULL;
+	}
+	if (parse->textures[EA].tex)
+	{
+		mlx_delete_texture(parse->textures[EA].tex);
+		parse->textures[EA].tex = NULL;
+	}
+}
+
+void	close_game(void *ptr)
+{
+	t_data	*data;
+
+	data = (t_data *)ptr;
+	if (data && data->parse)
+		destroy_textures(data->parse);
+	if (data && data->img && data->mlx_ptr)
+		mlx_delete_image(data->mlx_ptr, data->img);
+	if (data && data->mlx_ptr)
+		mlx_terminate(data->mlx_ptr);
+	exit(0);
+}
+
+int	check_path_tex(t_parse *parse)
+{
+	if (!parse->textures[NO].path || !parse->textures[NO].path[0])
+		return (ft_putstr_fd("Error: NO texture path is NULL or empty\n", 2), 1);
+	if (!parse->textures[SO].path || !parse->textures[SO].path[0])
+		return (ft_putstr_fd("Error: SO texture path is NULL or empty\n", 2), 1);
+	if (!parse->textures[WE].path || !parse->textures[WE].path[0])
+		return (ft_putstr_fd("Error: WE texture path is NULL or empty\n", 2), 1);
+	if (!parse->textures[EA].path || !parse->textures[EA].path[0])
+		return (ft_putstr_fd("Error: EA texture path is NULL or empty\n", 2), 1);
+	return (0);
+}
+
+int	load_texture(t_parse *parse)
+{
+	if (!parse || check_path_tex(parse))
+		return (1);
+	parse->textures[NO].tex = mlx_load_png(parse->textures[NO].path);
+	if (!parse->textures[NO].tex)
+		return (ft_putstr_fd("Error: Invalid NO texture\n", 2), 1);
+	parse->textures[SO].tex = mlx_load_png(parse->textures[SO].path);
+	if (!parse->textures[SO].tex)
+	{
+		destroy_textures(parse);
+		return (ft_putstr_fd("Error: Invalid SO texture\n", 2), 1);
+	}
+	parse->textures[WE].tex = mlx_load_png(parse->textures[WE].path);
+	if (!parse->textures[WE].tex)
+	{
+		destroy_textures(parse);
+		return (ft_putstr_fd("Error: Invalid WE texture\n", 2), 1);
+	}
+	parse->textures[EA].tex = mlx_load_png(parse->textures[EA].path);
+	if (!parse->textures[EA].tex)
+	{
+		destroy_textures(parse);
+		return (ft_putstr_fd("Error: Invalid EA texture\n", 2), 1);
+	}
+	return (0);
+}
---
+++ raycasting/mlx_func.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_func.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbayousf <cbayousf@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/08 13:17:41 by cbayousf          #+#    #+#             */
+/*   Updated: 2025/10/30 10:43:40 by cbayousf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub.h"
+
+void	initialisation_mlx(t_data *data)
+{
+	data->mlx_ptr = mlx_init(data->screenwidth, data->screenheight,
+			"cub3D", true);
+	if (!data->mlx_ptr)
+		exit(1);
+	data->img = mlx_new_image(data->mlx_ptr, data->screenwidth,
+			data->screenheight);
+	if (!data->img)
+	{
+		mlx_terminate(data->mlx_ptr);
+		exit(1);
+	}
+	if (mlx_image_to_window(data->mlx_ptr, data->img, 0, 0) == -1)
+	{
+		mlx_terminate(data->mlx_ptr);
+		exit(1);
+	}
+}
+
+void	close_win(void	*ptr)
+{
+	t_data	*data;
+
+	data = (t_data *)ptr;
+	mlx_delete_image(data->mlx_ptr, data->img);
+	mlx_terminate(data->mlx_ptr);
+    destroy_textures(data->parse);
+	ft_malloc(0, "FREE");
+	exit(0);
+}
+
+int	key_release(t_data *data)
+{
+	if (mlx_is_key_down(data->mlx_ptr, MLX_KEY_W))
+		data->keys.up = 0;
+	if (mlx_is_key_down(data->mlx_ptr, MLX_KEY_S))
+		data->keys.down = 0;
+	if (mlx_is_key_down(data->mlx_ptr, MLX_KEY_A))
+		data->keys.a = 0;
+	if (mlx_is_key_down(data->mlx_ptr, MLX_KEY_D))
+		data->keys.d = 0;
+	if (mlx_is_key_down(data->mlx_ptr, MLX_KEY_LEFT))
+		data->keys.left = 0;
+	if (mlx_is_key_down(data->mlx_ptr, MLX_KEY_RIGHT))
+		data->keys.right = 0;
+	return (0);
+}
+
+int	key_press(t_data *data)
+{
+	if (mlx_is_key_down(data->mlx_ptr, MLX_KEY_ESCAPE))
+		close_win(data);
+	if (mlx_is_key_down(data->mlx_ptr, MLX_KEY_W))
+		data->keys.up = 1;
+	if (mlx_is_key_down(data->mlx_ptr, MLX_KEY_S))
+		data->keys.down = 1;
+	if (mlx_is_key_down(data->mlx_ptr, MLX_KEY_A))
+		data->keys.a = 1;
+	if (mlx_is_key_down(data->mlx_ptr, MLX_KEY_D))
+		data->keys.d = 1;
+	if (mlx_is_key_down(data->mlx_ptr, MLX_KEY_LEFT))
+		data->keys.left = 1;
+	if (mlx_is_key_down(data->mlx_ptr, MLX_KEY_RIGHT))
+		data->keys.right = 1;
+	return (0);
+}
